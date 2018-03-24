9b03de97f2bdc7f300a7293113c83bfc4b4d1423
Yohei Kuchiki <extentlambda@gmail.com>
2018-03-25 04:25:55 +0900

    Update to version 25.3

--- build-emacs.sh
+++ build-emacs.sh
@@ -7,7 +7,7 @@
 
 set -eu
 
-readonly version="25.2"
+readonly version="25.3"
 
 if emacs --version | head -n 1 | grep -iE "GNU Emacs $version" ; then
     echo Emacs "$version" is already installed.
@@ -18,12 +18,16 @@ fi
 printf "Installing dependencies... "
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install -y stow build-essential libx11-dev xaw3dg-dev \
-     libjpeg-dev libpng12-dev libgif-dev libtiff5-dev libncurses5-dev \
+sudo apt-get install -y stow build-essential libx11-dev \
+     libjpeg-dev libgif-dev libtiff5-dev libncurses5-dev \
      libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
      libxml2-dev libgpm-dev libotf-dev libm17n-dev \
-     libgnutls-dev wget
+     libgtk-3-dev libwebkitgtk-3.0-dev libxpm-dev wget
 printf "done.\n"
+
+# from Ubuntu 16.10, libgnutls-dev, libpng12-dev is no longer available
+sudo apt-get install libgnutls-dev libpng12-dev || \
+		 sudo apt-get install libgnutls28-dev libpng-dev
 
 # download source package
 cd ~/Downloads
@@ -39,7 +43,9 @@ echo "Building and installing GNU Emacs... "
 sudo mkdir -p /usr/local/stow
 cd emacs-"$version"
 
-./configure
+./configure \
+		--with-xft \
+		--with-x-toolkit=gtk3
 make
 sudo make install prefix=/usr/local/stow/emacs-"$version"
 
